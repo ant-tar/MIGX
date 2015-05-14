d9732f6f30f1336d0f38a7e76c540590ff657b95
Anton <himurovich@gmail.com>
2015-05-14 12:45:48 +0300

    Update export.php
    75 row has duplicate $sort definition

--- core/components/migx/processors/mgr/default/export.php
+++ core/components/migx/processors/mgr/default/export.php
@@ -72,7 +72,6 @@ if (!($scriptProperties['download'])) {
 
     /* setup default properties */
     $sort = $modx->getOption('sort', $scriptProperties, 'id');
-    $sort = $modx->getOption('sort', $scriptProperties, 'id');
     $dir = $modx->getOption('dir', $scriptProperties, 'ASC');
     $showtrash = $modx->getOption('showtrash', $scriptProperties, '');
 
